25c4bf1d9aeab86dc700d0bc27777ca2ff0aa4c5
Dima-Penzev <dima.1301@mail.ru>
2021-08-07 10:38:03 +0300

    исправил недочеты

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,15 +2,7 @@ import PropTypes from "prop-types";
 import defaultAvatar from "./defaultAvatar.jpg";
 import s from "./Profile.module.css";
 
-function Profile({
-  name,
-  tag,
-  location,
-  avatar = defaultAvatar,
-  followers,
-  views,
-  likes,
-}) {
+function Profile({ name, tag, location, avatar = defaultAvatar, stats }) {
   return (
     <div className={s.profile}>
       <div className={s.description}>
@@ -28,15 +20,15 @@ function Profile({
       <ul className={s.stats}>
         <li className={s.item}>
           <span className={s.label}>Followers</span>
-          <span className={s.quantity}>{followers}</span>
+          <span className={s.quantity}>{stats.followers}</span>
         </li>
         <li className={s.item}>
           <span className={s.label}>Views</span>
-          <span className={s.quantity}>{views}</span>
+          <span className={s.quantity}>{stats.views}</span>
         </li>
         <li className={s.item}>
           <span className={s.label}>Likes</span>
-          <span className={s.quantity}>{likes}</span>
+          <span className={s.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
@@ -48,9 +40,11 @@ Profile.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  followers: PropTypes.number.isRequired,
-  views: PropTypes.number.isRequired,
-  likes: PropTypes.number.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
 
 export default Profile;
